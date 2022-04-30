2104ba256be734cc9e011e8eaf4a4694b95899ff
Hyunmaa <fredley.infor@gmail.com>
2022-04-30 15:29:27 +0700

    Upload Project

---
+++ src/main/java/com/java/edu/Main.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package com.java.edu;
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
