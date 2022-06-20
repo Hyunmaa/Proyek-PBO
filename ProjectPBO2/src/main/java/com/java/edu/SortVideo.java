d79802291fe66215784682417737e67b8e32a2a1
Hyunmaa <fredley.infor@gmail.com>
2022-06-21 05:50:22 +0700

    Fix bug sort video (add try catch)

--- ProjectPBO2/src/main/java/com/java/edu/SortVideo.java
+++ ProjectPBO2/src/main/java/com/java/edu/SortVideo.java
@@ -215,7 +215,11 @@ public class SortVideo extends javax.swing.JFrame {
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new SortVideo().setVisible(true);
+                try {
+                    new SortVideo().setVisible(true);
+                } catch (IOException ex) {
+                    Logger.getLogger(SortVideo.class.getName()).log(Level.SEVERE, null, ex);
+                }
             }
         });
     }
